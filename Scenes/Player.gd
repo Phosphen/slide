a7e8e8de6e634a1997f2a167c62a8a8e86cd34de
IMGSaibh <murmelauge87@gmx.de>
2023-11-12 17:57:11 +0100

    PH-7 : Jump higher after run for while

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -4,10 +4,10 @@ extends CharacterBody2D
 const SPEED = 600.0
 # 2D Engines negative is up and positive is down
 const JUMP_VELOCITY = -500.0
-const ACCELERATION = 1400
+const ACCELERATION = 700
 const FRICTION = 700
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
@@ -29,6 +29,8 @@ func handle_jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_pressed("ui_accept") && (velocity.x > 590 || velocity.x < -590):
+			velocity.y = JUMP_VELOCITY * 2.5
 	else:
 		# for short jumps and prevent flying by push jump multiple times
 		if Input.is_action_just_released("ui_accept") and (velocity.y < JUMP_VELOCITY / 2):
@@ -41,3 +43,6 @@ func apply_friction(input_axis, delta):
 func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		print(velocity.x)
+
+
