8d098984029ec90b7bb3f50b99043a095b31cc35
Artem Dyadichkin <artempolybreaker@gmail.com>
2023-11-27 00:05:53 +0100

    chore: fixed platform placement (no overlaps with walls/other platforms)

--- LevelGen/Main.gd
+++ LevelGen/Main.gd
@@ -14,6 +14,8 @@ var screen_width
 var middle
 var wall_index
 
+var wall_width:float = 0.0
+
 func _ready():
 #	screen_size = get_viewport().size
 	screen_size = Vector2(800, 600)
@@ -34,10 +36,10 @@ func _spawn_floor():
 		add_child(floor_instance)
 
 func _spawn_level_batch():
-	for i in range(20):
-		_spawn_platform(i)
 	for i in range(10):
 		_spawn_walls(wall_index + i)
+	for i in range(3, 20):
+		_spawn_platform(i)
 	wall_index += 10
 
 func _spawn_platform(index):
@@ -48,10 +50,14 @@ func _spawn_platform(index):
 
 	var from = last_platform_position.x - sprite_size.x * 0.5 - MAX_OFFSET
 	var to = last_platform_position.x + sprite_size.x * 0.5 + MAX_OFFSET
+	# make sure there are no overlaps with the walls
+	from = max(from - sprite_size.x * 0.5, MARGIN + wall_width * 0.5 + sprite_size.x * 0.5)
+	to = min(to + sprite_size.x * 0.5, screen_width - MARGIN - wall_width * 0.5 - sprite_size.x * 0.5)
+	
 	from = clampf(from, MARGIN, screen_width - MARGIN)
 	to = clampf(to, MARGIN, screen_width - MARGIN)
 	var x_pos = randf_range(from, to)
-	var y_pos = last_platform_position.y - sprite_size.y / 2 - DISTANCE_BETWEEN_PLATFORMS * index;
+	var y_pos = last_platform_position.y - sprite_size.y * 0.5 - DISTANCE_BETWEEN_PLATFORMS * index;
 
 	platform.position = Vector2(x_pos, y_pos)
 	last_platform_position = platform.position
@@ -61,6 +67,8 @@ func _spawn_walls(index):
 	var wallLeft = wall_scene.instantiate()
 	var wallRight = wall_scene.instantiate()
 	var sprite_size = _get_sprite_size(wallLeft)
+	
+	wall_width = sprite_size.x
 	
 	# Y component is aka width in this context, because the sprite is rotated
 	var posLeft = Vector2(MARGIN, screen_size.y - index * sprite_size.y)
@@ -75,7 +83,6 @@ func _get_sprite_size(node_with_sprite):
 	var sprite_w = sprite_node.texture.get_width()
 	var sprite_h = sprite_node.texture.get_height()
 	return Vector2(sprite_w, sprite_h)
-
 
 func _on_player_reached_top():
 	_spawn_level_batch()
