2c12ed1a5341eb5552b9ad40a9d06df0e5622178
Franky <frank@team-frank.net>
2023-12-02 23:01:19 +0100

    tidy up

--- Scripts/Levels/main_menu.gd
+++ Scripts/Levels/main_menu.gd
@@ -1,16 +1,8 @@
 extends Control
 
-#Data
-const CROSSHAIR : CompressedTexture2D = preload("res://Textures/UI/Crosshair.png")
-const MOUSE : CompressedTexture2D = preload("res://Textures/UI/Mouse.png")
-const CROSSHAIR_OFFSET : Vector2 = Vector2(12, 12)
-const MOUSE_OFFSET : Vector2 = Vector2(0, 0)
 @onready var parallax_background: ParallaxBackground = $ParallaxBackground
 
-#Refrences
-
 func _ready():
-	DisplayServer.cursor_set_custom_image(MOUSE, DisplayServer.CURSOR_ARROW, MOUSE_OFFSET)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _process(_delta):
@@ -24,7 +16,6 @@ func _on_quit_mouse_entered():
 
 func _on_start_button_up():
 	AudioManager.play_sound(AudioManager.MENU_CLICK)
-	DisplayServer.cursor_set_custom_image(CROSSHAIR, DisplayServer.CURSOR_ARROW, CROSSHAIR_OFFSET)
 	get_tree().change_scene_to_file("res://LevelGen/main.tscn")
 
 func _on_quit_button_up():
