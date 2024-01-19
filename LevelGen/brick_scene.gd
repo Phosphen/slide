4ea6b16521e133647f395c5d16bfe226e3a0d26f
meralium <polybreaker@gmail.com>
2024-01-19 23:02:04 +0100

    chore: self destroy the bricks after timer

--- LevelGen/brick_scene.gd
+++ LevelGen/brick_scene.gd
@@ -1,5 +1,12 @@
 extends RigidBody2D
 
 func _on_timer_timeout():
+	# make the brick fall down
 	sleeping = false
 	freeze = false
+	# self destroy after new timer
+	var timer = Timer.new()
+	self.add_child(timer)
+	timer.connect("timeout", queue_free)
+	timer.set_wait_time(3)
+	timer.start()
