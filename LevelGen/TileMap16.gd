8bc7f03ca72b6ba83f6730c0afcec6e0b5a30fe9
meralium <polybreaker@gmail.com>
2024-01-28 19:11:18 +0100

    feat: first refactoring of level-gen

--- LevelGen/TileMap16.gd
+++ LevelGen/TileMap16.gd
@@ -1,11 +1,22 @@
 extends TileMap
 
-
+var wall_index = 0
+const distance_between_platforms = 4
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_spawn_level_batch()
 
+func _spawn_level_batch():
+	for i in range(20):
+		_spawn_platform(i)
+	#wall_index += 10
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# xRange: 5 <-> 24
+func _spawn_platform(index):
+	var randomX = randi_range(7, 22)
+	index *= distance_between_platforms
+	set_cell(0, Vector2i(randomX - 2, index * -1), 0, Vector2i(64, 0))
+	set_cell(0, Vector2i(randomX - 1, index * -1), 0, Vector2i(65, 0))
+	set_cell(0, Vector2i(randomX - 0, index * -1), 0, Vector2i(66, 0))
+	set_cell(0, Vector2i(randomX + 1, index * -1), 0, Vector2i(67, 0))
+	set_cell(0, Vector2i(randomX + 2, index * -1), 0, Vector2i(68, 0))
--- LevelGen/TileMap64.gd
+++ LevelGen/TileMap64.gd
@@ -1,10 +1,28 @@
 extends TileMap
 
-
+var wall_index = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_cell(0, Vector2i(2, 2), 2, Vector2i(0, 0))
+	_spawn_level_batch()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func _spawn_level_batch():
+	for i in range(10):
+		_spawn_walls(wall_index + i)
+	for i in range(20):
+		_spawn_platform(i)
+	#wall_index += 10
+	
+func _spawn_walls(index):
+	set_cell(0, Vector2i(0, index * -1), 2, Vector2i(0, 0))
+	set_cell(0, Vector2i(6, index * -1), 2, Vector2i(0, 0))
+	
+	set_cell(0, Vector2i(-1, index * -1), 2, Vector2i(4, 0))
+	set_cell(0, Vector2i(7, index * -1), 2, Vector2i(4, 0))
+	pass
+
+func _spawn_platform(index):
 	pass
