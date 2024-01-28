a4d1616cb406c2b299b3342d02aecf1178cfe0c7
meralium <polybreaker@gmail.com>
2024-01-28 19:23:42 +0100

    feat: endless gen works again

--- LevelGen/TileMap16.gd
+++ LevelGen/TileMap16.gd
@@ -1,17 +1,18 @@
 extends TileMap
 
-var wall_index = 0
-const distance_between_platforms = 4
+var platform_index = 0
+const distance_between_platforms = 6
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_spawn_level_batch()
+	pass
+	#_spawn_next_batch()
 
-func _spawn_level_batch():
+func _spawn_next_batch():
 	for i in range(20):
-		_spawn_platform(i)
-	#wall_index += 10
+		_spawn_platform(platform_index + i)
+	platform_index += 20
 
-# xRange: 5 <-> 24
+# xRange wall to wall: 5 <-> 24
 func _spawn_platform(index):
 	var randomX = randi_range(7, 22)
 	index *= distance_between_platforms
@@ -20,3 +21,7 @@ func _spawn_platform(index):
 	set_cell(0, Vector2i(randomX - 0, index * -1), 0, Vector2i(66, 0))
 	set_cell(0, Vector2i(randomX + 1, index * -1), 0, Vector2i(67, 0))
 	set_cell(0, Vector2i(randomX + 2, index * -1), 0, Vector2i(68, 0))
+
+func _on_player_reached_top():
+	# todo: clear lower batch
+	_spawn_next_batch()
--- LevelGen/TileMap64.gd
+++ LevelGen/TileMap64.gd
@@ -1,28 +1,26 @@
 extends TileMap
 
 var wall_index = 0
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	_spawn_level_batch()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+	#_spawn_next_batch()
+	#_spawn_level_batch()
+	#_spawn_level_batch()
 
-func _spawn_level_batch():
+func _spawn_next_batch():
 	for i in range(10):
 		_spawn_walls(wall_index + i)
-	for i in range(20):
-		_spawn_platform(i)
-	#wall_index += 10
-	
+	wall_index += 10
+
 func _spawn_walls(index):
 	set_cell(0, Vector2i(0, index * -1), 2, Vector2i(0, 0))
 	set_cell(0, Vector2i(6, index * -1), 2, Vector2i(0, 0))
 	
 	set_cell(0, Vector2i(-1, index * -1), 2, Vector2i(4, 0))
 	set_cell(0, Vector2i(7, index * -1), 2, Vector2i(4, 0))
-	pass
 
-func _spawn_platform(index):
-	pass
+
+func _on_player_reached_top():
+	# todo: clear lower batch
+	_spawn_next_batch()
