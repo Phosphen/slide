e54b72475c4ee505bf29d5d03ecb09ec737eb860
meralium <polybreaker@gmail.com>
2024-02-04 16:12:18 +0100

    feat: double width platforms work with new spawner

--- LevelGen/TileMap16.gd
+++ LevelGen/TileMap16.gd
@@ -2,7 +2,9 @@ extends TileMap
 
 var platform_index = 0
 const distance_between_platforms = 6
-# Called when the node enters the scene tree for the first time.
+# Probability of spawning a double-width platform
+var double_width_probability = 0.5  # 20% chance
+
 func _ready():
 	_spawn_next_batch()
 
@@ -13,9 +15,16 @@ func _spawn_next_batch():
 
 # xRange wall to wall: 5 <-> 24
 func _spawn_platform(index):
-	var randomX = randi_range(7, 22)
 	index *= distance_between_platforms
-	set_cell(0, Vector2i(randomX - 0, index * -1), 2, Vector2i(0, 0), 2)
+	
+	# Determine if this platform should be double-width
+	var is_double_width = randf() < double_width_probability
+	if is_double_width:
+		var randomX = randi_range(9, 20)
+		set_cell(0, Vector2i(randomX - 0, index * -1), 2, Vector2i(0, 0), 4)
+	else:
+		var randomX = randi_range(7, 22)
+		set_cell(0, Vector2i(randomX - 0, index * -1), 2, Vector2i(0, 0), 2)
 
 func _on_player_reached_top():
 	# todo: clear lower batch
