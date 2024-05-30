7de0ccdf79d713082f56de89c4533fe320e74ee0
Franky <frank@team-frank.net>
2024-05-30 12:01:06 +0200

    stop music when leaving menu

--- Scripts/Levels/game_over_scene.gd
+++ Scripts/Levels/game_over_scene.gd
@@ -11,3 +11,6 @@ func _ready():
 func _process(delta):
 	parallax_background.scroll_offset = parallax_background.scroll_offset + Vector2(0, -FALL_SPEED * 0.25)
 	spinning_player.position += Vector2(0, delta * FALL_SPEED)
+
+func _exit_tree():
+	AudioManager.stop_music(AudioManager.BEAKGROUND_MUSIC_Game_Over_Theme)
